de5224d52ab334671788e45fd7ada1909f43f669
Umaid Abbasi <umaidabbasi@gmail.com>
2024-08-02 11:15:52 -0400

    chore(diagram): Update to reflect current infra

--- diagram.py
+++ diagram.py
@@ -6,24 +6,24 @@ from diagrams.onprem.storage import CephOsd # type: ignore
 from diagrams.k8s.infra import Master, Node
 
 with Diagram("Home Network Overview", show=True):
-    dmz_ingress = Nginx("DMZ Ingress")
+    dmz_ingress = Nginx("DMZ Ingress: 192.168.11.5")
     opnsense = Opnsense("OpnSense")
-    multi_gig_switch = Internet("2.5G Switch Lan 1")
+    multi_gig_switch = Internet("1G Switch Lan 1")
     with Cluster("UnRaid"):
         unraid = [
             Docker("Containers"),
             CephOsd("Storage"),
             Server("VMs")]
     
-    one_gb_switch = Internet("1G Switch Lan k8")
+    one_gb_switch = Internet("2.5G Switch Lan k8")
     with Cluster("k8s"):
         k8s = [
             Master("Control 1"),
             Master("Control 2"),
-            Node("Worker 1"),
-            Node("Worker 2"),
-            Node("Worker 2")]
+            Master("Control 3"),
+        ]
+
 
     dmz_ingress >> opnsense >>  Edge(label="192.168.1.x") >> multi_gig_switch >> unraid
     dmz_ingress >> opnsense >> Edge(label="192.168.2.x") >> one_gb_switch >> k8s
-    dmz_ingress << Edge(label="192.168.11.x") << unraid[0]
+    dmz_ingress << Edge(label="DMZ: 192.168.11.x") << unraid[0]
